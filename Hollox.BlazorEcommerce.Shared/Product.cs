bda2709a076fa14eb5be15b1345df925eafc188b
abcdefgh <>
2022-11-28 09:30:46 -0500

    feature: add migration for productType and productVariant

--- Hollox.BlazorEcommerce.Shared/Product.cs
+++ Hollox.BlazorEcommerce.Shared/Product.cs
@@ -9,9 +9,8 @@ public class Product
     public string Description { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
 
-    [Column(TypeName = "decimal(18,2)")]
-    public decimal Price { get; set; }
-
     public Category? Category { get; set; }
     public int CategoryId { get; set; }
+
+    public List<ProductVariant> Variants { get; set; } = new();
 }
---
+++ Hollox.BlazorEcommerce.Shared/ProductVariant.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Hollox.BlazorEcommerce.Shared;
+
+public class ProductVariant
+{
+    [JsonIgnore]
+    public Product Product { get; set; }
+    public int ProductId { get; set; }
+
+    public ProductType ProductType { get; set; }
+    public int ProductTypeId { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal OriginalPrice { get; set; }
+
+    public bool IsVisible { get; set; } = true;
+    public bool IsDeleted { get; set; } = false;
+    [NotMapped]
+    public bool IsEditing { get; set; } = false;
+    [NotMapped]
+    public bool IsNew { get; set; } = false;
+}
