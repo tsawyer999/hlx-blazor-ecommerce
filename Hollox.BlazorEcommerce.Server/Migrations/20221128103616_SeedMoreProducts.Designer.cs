c0fa5ce42259da396832311ed5f516501a016e2f
abcdefgh <>
2022-11-28 05:37:16 -0500

    feature: add more products

--- Hollox.BlazorEcommerce.Server/Migrations/20221128103616_SeedMoreProducts.Designer.cs
+++ Hollox.BlazorEcommerce.Server/Migrations/20221128103616_SeedMoreProducts.Designer.cs
@@ -3,6 +3,7 @@ using Hollox.BlazorEcommerce.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hollox.BlazorEcommerce.Server.Migrations
 {
     [DbContext(typeof(ECommerceDataContext))]
-    partial class ECommerceDataContextModelSnapshot : ModelSnapshot
+    [Migration("20221128103616_SeedMoreProducts")]
+    partial class SeedMoreProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
