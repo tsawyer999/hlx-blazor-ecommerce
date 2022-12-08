fb1c0e181f722d6ad1fc49317ce3366b244a6ef3
abcdefgh <>
2022-12-08 11:02:53 -0500

    feature: display variant price

--- Hollox.BlazorEcommerce.Client/Pages/ProductDetails.razor.cs
+++ Hollox.BlazorEcommerce.Client/Pages/ProductDetails.razor.cs
@@ -8,6 +8,7 @@ public partial class ProductDetails
 {
     protected Product? Product;
     protected int CurrentTypeId;
+    protected ProductVariant? SelectedVariant;
 
     [Inject]
     private IProductService ProductService { get; set; } = default!;
@@ -20,5 +21,26 @@ public partial class ProductDetails
         Product = await ProductService.GetProductByIdAsync(Id);
         var firstVariant = Product?.Variants.FirstOrDefault();
         CurrentTypeId = firstVariant?.ProductTypeId ?? -1;
+    }
+
+    protected void OnProductVariantChange(ChangeEventArgs e)
+    {
+        if (Product == null)
+        {
+            return;
+        }
+
+        if (e.Value == null)
+        {
+            return;
+        }
+
+        if (!int.TryParse(e.Value.ToString(), out var selectedVariantId))
+        {
+            return;
+        }
+
+        SelectedVariant = Product.Variants.Find(v => v.ProductTypeId == selectedVariantId);
+        Console.WriteLine("It is definitely: " + SelectedVariant);
     }
 }
---
+++ Hollox.BlazorEcommerce.Shared/UserRegister.cs
@@ -0,0 +1,8 @@
+﻿namespace Hollox.BlazorEcommerce.Shared;
+
+internal class UserRegister
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+}
